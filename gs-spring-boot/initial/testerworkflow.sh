e04639093cbf39fd82b08ef26159a9287cf8cce1
gchandragithub <52721152+gchandragithub@users.noreply.github.com>
2019-09-10 17:03:06 -0400

    Add files via upload

--- gs-spring-boot/initial/testerworkflow.sh
+++ gs-spring-boot/initial/testerworkflow.sh
@@ -6,4 +6,4 @@ export ENVIRONMENT_STAGEPROD=8088
 sleep 1
 ./gs-spring-boot/initial/gradlew build
 java -jar /Users/Shared/Jenkins/Home/jenkinsrepo/gs-spring-boot/initial/build/libs/gs-spring-boot-0.1.0.jar
-./gs-spring-boot/initial/test2stage.sh
+#./gs-spring-boot/initial/test2stage.sh
