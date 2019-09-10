187a8b088e733177daaf206e5c2f7b77479a9368
gchandragithub <52721152+gchandragithub@users.noreply.github.com>
2019-09-10 10:35:38 -0400

    Add files via upload

--- gs-spring-boot/initial/testerworkflow.sh
+++ gs-spring-boot/initial/testerworkflow.sh
@@ -4,4 +4,4 @@ curl -X POST localhost:8088/actuator/shutdown
 sleep 5
 export ENVIRONMENT_STAGEPROD=8088
 sleep 1
-./gradlew build && java -jar build/libs/gs-spring-boot-0.1.0.jar&
+./gs-spring-boot/initialgradlew build && java -jar build/libs/gs-spring-boot-0.1.0.jar&
