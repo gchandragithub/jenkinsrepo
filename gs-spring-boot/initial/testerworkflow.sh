bb18344516695cb8a29c2ff24c910a3fd32ffb21
gchandragithub <52721152+gchandragithub@users.noreply.github.com>
2019-09-18 12:12:09 -0400

    Add files via upload

--- gs-spring-boot/initial/testerworkflow.sh
+++ gs-spring-boot/initial/testerworkflow.sh
@@ -5,7 +5,5 @@ sleep 5
 export ENVIRONMENT_STAGEPROD=8088
 sleep 1
 ./gs-spring-boot/initial/gradlew build
-docker build --build-arg environment=8088 -t helloworld .
-docker run -p 8088:8088 helloworld:latest
-#java -jar /Users/Shared/Jenkins/Home/jenkinsrepo/gs-spring-boot/initial/build/libs/gs-spring-boot-0.1.0.jar
+java -jar /Users/Shared/Jenkins/Home/jenkinsrepo/gs-spring-boot/initial/build/libs/gs-spring-boot-0.1.0.jar
 #./gs-spring-boot/initial/test2stage.sh
